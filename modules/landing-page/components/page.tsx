d2693a0dc35d4974e1999b304e42959fab04f4e0
LENOVO <yudriqul20803@gmail.com>
2026-05-26 10:18:15 +0700

    fix: resolve window is not defined error in react-leaflet

--- modules/landing-page/components/page.tsx
+++ modules/landing-page/components/page.tsx
@@ -1,13 +1,16 @@
-import React from "react";
-import Navbar from "./Navbar";
-import HeroSection from "./HeroSection";
-import FeaturesSection from "./FeaturesSection";
+import dynamic from "next/dynamic";
 import AISpotlightSection from "./AISpotlightSection";
-import LandMapSection from "./LandMapSection";
-import NewsletterSection from "./NewsletterSection";
+import FeaturesSection from "./FeaturesSection";
 import Footer from "./footer";
+import HeroSection from "./HeroSection";
 import HowItWorks from "./HowItWorks";
+import Navbar from "./Navbar";
+import NewsletterSection from "./NewsletterSection";
 
+const LandMapSection = dynamic(
+  () => import("@/modules/landing-page/components/LandMapSection"),
+  { ssr: false },
+);
 export const LandingPage = () => {
   return (
     <div className="min-h-screen bg-[#FDFDFD] text-slate-900 selection:bg-green-100 font-sans">
